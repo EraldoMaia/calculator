46de5359726a580c16511c58e3397e87da8423c8
Eraldo Henrique Maia <55844931+EraldoMaia@users.noreply.github.com>
2020-06-09 11:06:11 -0300

    alt 2

--- App.js
+++ App.js
@@ -16,22 +16,22 @@ export default class App extends Component {
         <Display value={this.state.displayValue} />
         <View style={styles.buttons}>
           <Button label='AC' triple onClick={this.clearMemory}></Button>
-          <Button label='/' operation onClick={() => this.setOperation('/')}></Button>
+          <Button label='/' operation onClick={this.setOperation}></Button>
           <Button label='7' onClick={this.addDigit}></Button>
           <Button label='8' onClick={this.addDigit}></Button>
           <Button label='9' onClick={this.addDigit}></Button>
-          <Button label='*' operation onClick={() =>this.setOperation('*')}></Button>
+          <Button label='*' operation onClick={this.setOperation}></Button>
           <Button label='4' onClick={this.addDigit}></Button>
           <Button label='5' onClick={this.addDigit}></Button>
           <Button label='6' onClick={this.addDigit}></Button>
-          <Button label='-' operation onClick={() =>this.setOperation('-')}></Button>
+          <Button label='-' operation onClick={this.setOperation}></Button>
           <Button label='1' onClick={this.addDigit}></Button>
           <Button label='2' onClick={this.addDigit}></Button>
           <Button label='3' onClick={this.addDigit}></Button>
-          <Button label='+' operation onClick={() =>this.setOperation('+')}></Button>
+          <Button label='+' operation onClick={this.setOperation}></Button>
           <Button label='0' double onClick={this.addDigit}></Button>
           <Button label='.' onClick={this.addDigit}></Button>
-          <Button label='=' operation onClick={() => this.setOperation('=')}></Button>
+          <Button label='=' operation onClick={this.setOperation}></Button>
         </View>
       </View>
     )
